e8c1ec9ab4a1476410035bc71318a53d295ea53a
Christian Ragonese <christianragonese1313@gmail.com>
2025-06-20 15:46:38 +0200

    save

--- src/project/Main.java
+++ src/project/Main.java
@@ -12,6 +12,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int num=0;
+        do{
+        System.out.println("Inserisci il numero di elementi multimediali che desideri aggiungere tra 1 e 5");
+        num = Integer.parseInt(scanner.nextLine());
+        while(num <1 || num>5);
+        for(int i=0; i<num; i++) {
+            String text = "";
+            do {
+                System.out.println("Inserisci la tipologia di file tra Audio, Video, Immagine del file numero : " + num);
+                text = scanner.nextLine().toUpperCase();
+            } while (!text.equals("AUDIO") && !text.equals("VIDEO") && !text.equals("IMMAGINE"));
+
+            System.out.println();
+        }
 
         Audio Audio1 = new Audio("Audio 1", 4, 5);
         Video Video1 = new Video("Video 1", 3, 5, 7);
